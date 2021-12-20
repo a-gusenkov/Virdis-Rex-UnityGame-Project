ee690e6ebd02adcdbf0577eb8b5a3f7b3f770f1a
Anastasia Gusenkov <anastasiagusenkov1@gmail.com>
2021-12-20 02:06:12 -0500

    Revert "Master"

--- Assets/Scripts/Levels.cs
+++ Assets/Scripts/Levels.cs
@@ -6,26 +6,17 @@ using UnityEngine.SceneManagement;
 
 public class Levels : MonoBehaviour
 {
-    public Text leafPointText;
-    public Text pointText;
-    public Text moneyText;
+    [SerializeField] Text leafPointText;
     
     //leaf variable is general leaf point tracker of entire game
     //publicLeaf is the point tracker player sees
     public int leaf = 0;
-    int point = 0;
-    int money = 3;
-    
-    
+    public int publicLeaf;
+    public int green;
     
     // Start is called before the first frame update
     void Start()
     {
-        pointText.text = "Green Points : " + PlayerPrefs.GetInt("points", point).ToString();
-        Debug.Log(point);
-        
-        moneyText.text = "Money : " + PlayerPrefs.GetInt("money", money).ToString();
-        Debug.Log(money);
         
         leafPointText.text = "Leaves : " + leaf;
        DisplayScore();
@@ -44,69 +35,16 @@ public class Levels : MonoBehaviour
     }
     //in the future we can try making a hidden counter
     //for total points via leaf varibale
-    public void AddPoints(){
-        leaf += 1;
-        
-        leafPointText.text = "Leaves : " + leaf;
+    public void AddPoints(int points){
+        leaf += points;
+        publicLeaf += points;
         DisplayScore();
         if(leaf == 5){
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-            leafPointText.text = "Leaves : " + leaf;
+            publicLeaf = 0;
         }
-      
-    }
-
-
-    public void AddPoint(){
-
-        point = PlayerPrefs.GetInt("points");
-        point = point + 1;
-        PlayerPrefs.SetInt("points", point);
-
-    }
-    public void AddMoney(){
-        money = PlayerPrefs.GetInt("money");
-        money += 1;
-        PlayerPrefs.SetInt("money", money);
-
-    }
-    public void LosePoint(){
-
-        point = PlayerPrefs.GetInt("points");
-        point = point - 1;
-        PlayerPrefs.SetInt("points", point);
-    }
-    public void LoseMoney(){
-        money = PlayerPrefs.GetInt("money");
-        money = money - 1;
-
-        // if( money == 0){
-
-        // SceneManager.LoadScene("GameEndPoor");
-        // //     SceneManager.LoadScene("Stage");
-        // //     //PlayerPrefs.DeleteAll();
-        // }
-        PlayerPrefs.SetInt("money", money);
-    }
-
-    public void checkMoneyZero(){
-         money = PlayerPrefs.GetInt("money");
-         if (money == 0){
-             SceneManager.LoadScene("EndScreenPoor"); 
-             resertAllLevels();
-         }
-
-    }
-
-    public void resertAllLevels(){
-   
-     //points
-     int m = 3;
-     int p = 0;
-     PlayerPrefs.SetInt("money", m);
-     PlayerPrefs.SetInt("points", p);
-     moneyText.text = "Money : " + m.ToString();
-     pointText.text = "Green Points : " + p.ToString();
-
+        if(leaf == 10){
+            SceneManager.LoadScene("LevelText2");
+        }
     }
 }
